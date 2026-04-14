8b0f5ac3e0e1b75b5b31a7af5a6286b77fa028cf
Daniel <dw@vy99.de>
2026-04-14 16:58:15 +0200

    v1065 – Fix: Teilnahmen-Tabelle – erstes Jahr auf Mobile nicht mehr abgeschnitten (display:inline-block → overflow-x:auto;width:100%)

--- htdocs/js/10_veranstaltungen.js
+++ htdocs/js/10_veranstaltungen.js
@@ -770,7 +770,7 @@ async function _loadSerieTeilnahmen(serieId) {
     return '<th style="text-align:center;width:38px;min-width:38px;font-size:11px;font-weight:600;color:var(--text2);padding:4px 2px">' + j + '</th>';
   }).join('');
 
-  var html = '<div class="table-scroll" style="display:inline-block;max-width:100%"><table class="rek-table" style="width:auto">';
+  var html = '<div class="table-scroll" style="overflow-x:auto;width:100%"><table class="rek-table" style="width:auto;min-width:max-content">';
   html += '<thead><tr>' +
     '<th style="width:34px"></th>' +
     '<th style="text-align:left;min-width:140px">Athlet*in</th>' +
